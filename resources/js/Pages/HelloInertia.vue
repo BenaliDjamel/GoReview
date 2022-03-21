d78a9b307a11672c891fbf98eb67c9fa73ed5fbf
Djamel Benali <djamelbenalidev@gmail.com>
2022-03-21 19:08:28 +0100

    init

---
+++ resources/js/Pages/HelloInertia.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+import {Link} from '@inertiajs/inertia-vue3';
+
+const props = defineProps(['name'])
+
+</script>
+
+<template>
+ <div>
+     Hello inertia - {{props.name}}
+
+     <div class="mt-4">
+         <Link href="/" >Welcome</Link>
+     </div>
+ </div>
+</template>
